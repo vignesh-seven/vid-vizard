dd93a39236168e220a24910ad95829ba9b755e3e
Vignesh <vignesh77024@gmail.com>
2024-08-20 20:11:35 +0530

    Added some options for video conversion

--- src/App.tsx
+++ src/App.tsx
@@ -89,15 +89,33 @@ function App() {
         <video controls ref={videoPlayerRef}></video>
       </div>
       <div className="options">
-        <button>Preset</button>
-        <button>Format</button>
-        <button>Resolution</button>
-        <button>Framerate</button>
-        <button>Rotation</button>
-        <button>Crop</button>
-        <button>Video Settings</button>
-        <button>Audio Settings</button>
-        <button>Subtitle Settings</button>
+        <h4>Preset</h4>
+        <h4>Format</h4>
+        <select name="format" id="format">
+          <option value="same-as-source">Same as source</option>
+          <option value="mp4">MP4</option>
+          <option value="mkv">MKV</option>
+          <option value="webm">WebM</option>
+        </select>
+        <h4>Resolution</h4>
+        <select name="max-resolution" id="max-resolution">
+          <option value="same-as-source">Same as source</option>
+          <option value="4k">4K</option>
+          <option value="1080p">1080p</option>
+          <option value="720p">720p</option>
+        </select>
+        <h4>Framerate</h4>
+        <select name="framerate" id="framerate">
+          <option value="same-as-source">Same as source</option>
+          <option value="30">30</option>
+          <option value="60">60</option>
+          <option value="25">25</option>
+        </select>
+        <h4>Rotation</h4>
+        <h4>Crop</h4>
+        <h4>Video Settings</h4>
+        <h4>Audio Settings</h4>
+        <h4>Subtitle Settings</h4>
       </div>
       <div className="files-list">
         {/* <ul>
